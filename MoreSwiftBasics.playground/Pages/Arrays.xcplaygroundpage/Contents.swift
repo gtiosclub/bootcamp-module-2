2435f3c65de33a53bb8a1f14584916a178a4f013
Matthew Dong <mattdong1231@gmail.com>
2024-09-22 00:22:08 -0400

    Arrays

--- MoreSwiftBasics.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
+++ MoreSwiftBasics.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -125,24 +125,31 @@ var animalArray: [String] = ["Lion", "Zebra", "Elephant", "Turtle"]
 var optionalArray: [Int]?
 
 // [2] Is there a logical error with accessing index `4` in a 4-element array?
-print("4th element of animalArray: \(animalArray[4])")
+print("4th element of animalArray: \(animalArray[3])")
+//Yes, it is out of bounds and would cause a runtime error. I changed it to 3 instead.
 
 // [3] The operation below is quite dangerous. How can we make it safer?
-let unwrappedArray = optionalArray!
-print(unwrappedArray)
+if let unwrappedArray = optionalArray {
+    print(unwrappedArray)
+}
 
+//You can add an if statement to ensure that the nil case is covered and remove the ! to not forcefully unwrap the array.
 
 // [4] Initialize optionalArray
 /* BEGIN CODE */
-
+optionalArray = []
 /* END CODE */
 
 // [5, 6, 7] Add elements to optionalArray
 /* BEGIN CODE */
-
+optionalArray?.append(10)
+optionalArray?.append(contentsOf: [6, 7, 8, 9])
+optionalArray?.insert(55, at: 2)
 /* END CODE */
 
 // [8] Print the 5th element of optionalArray
 /* BEGIN CODE */
-
+if let unwrappedArray = optionalArray, unwrappedArray.count > 4 {
+    print(unwrappedArray[4])
+}
 /* END CODE */
